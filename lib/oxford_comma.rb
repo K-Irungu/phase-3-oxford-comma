f688d3a4942f5180505667cff8b9bd9953e13784
Kevin Irungu <kevin.irungu@student.moringaschool.com>
2022-10-19 15:56:51 +0300

    commit tree de26edbaee0d2b7df917ffc4a913c6dd1b0ad603

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+    x = array.length
+    if x == 1
+        array.join
+    elsif x ==2
+        array.filter.with_index{ |element, index| index != array.length - 1 }.push("and #{array[array.length - 1]}").join(" ")
+    else 
+        array.filter.with_index{ |element, index| index != array.length - 1 }.push("and #{array[array.length - 1]}").join(", ")
+    end
+    
 end
+
+
